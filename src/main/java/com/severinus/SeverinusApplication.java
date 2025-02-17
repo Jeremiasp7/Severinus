6706d738b414141c7d02d0eab74cb87472a39616
Jeremiasp7 <jeremiasp2004@gmail.com>
2025-02-17 11:16:03 -0300

    first commit

---
+++ src/main/java/com/severinus/SeverinusApplication.java
@@ -0,0 +1,13 @@
+package com.severinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverinusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverinusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/severinus/SeverinusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severinus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverinusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
